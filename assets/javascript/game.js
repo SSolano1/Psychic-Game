ee14fe19052c04cf80cd27a8ce5e59bee355e74c
Solon Solano <ssolano1@msn.com>
2018-07-19 22:43:25 -0400

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,86 +5,65 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 // Creating variables to hold the number of wins, losses, and ties.  They start at 0.
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
+var guessesLeft = 10;
 var guessedLetters = [];
 var letterToGuess = null;
-console.log ("solon")
+console.log("solon")
 
-  
+// Randomly chooses a choice from the options array.  This is the Computer's guess.
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    // Randomly chooses a choice from the options array.  This is the Computer's guess.
-   var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+//Allows 10 guesses
+function updateGuessesLeft() {
 
-    //Allows 10 guesses
-    function updateGuessesLeft() {
-        document.querySelector('#guessesLeft').innerHTML = "guesses left: " + guessesLeft;
-    };
+    document.querySelector('#guessesLeft').innerHTML = "guesses left: " + guessesLeft;
+};
 
-    function updateLetterToGuess() {
-        this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
-    };
-
-
-    function updateGuessesSoFar() {
+function updateGuessesSoFar() {
     // Display the letters selected by the user. 
-        document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
-    };
+    document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
 
-    // Function will be called when we reset everything
-    var reset = function () {
-        totalGuesses = 10;
-        guessesLeft = 10;
-        guessedLetters = [];
+function updateLetterToGuess() {
+    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
 
-        updateLetterToGuess();
-        updateGuessesLeft();
-        updateGuessesSoFar();
-    }
+var reset = function () {
+    guessesLeft = 10;
+    guessedLetters = [];
+    updateLetterToGuess();
+    updateGuessesSoFar();
 
-        updateLetterToGuess();
-        updateGuessesLeft();
+}
 
-    // This function runs whenever the user presses a key 
-    document.onkeyup = function (event) {
-    var userGuess = event.key; 
-    // var lowerGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    
+// This function runs whenever the user presses a key 
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+
     if (userGuess === computerGuess) {
         alert("That was Correct!");
-        
-    }
-    else if (userGuess !== computerGuess) {
-        guessesLeft--;
+        wins++;
+        document.querySelector('#wins').innerHTML = "Wins: " + wins;
         guessedLetters.push(userGuess);
-        updateGuessesLeft();
-        updateGuessesSoFar();
-
-    if (guessesLeft > 0) {
-            if (userGuess == letterToGuess) {
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                userGuess = userGuess.toUpperCase();
-                alert("Yes, you are psychic!");
-                reset();    
-    }        } else if (guessesLeft === 0) {
-        losses++;
-        document.querySelector('#losses').innerHTML = "Losses: " + losses;
-        alert("Sorry, you're not psychic, try again?");
-
-        // Start Over. 
+        updateGuessesSoFar(); //This prints out the array
         reset();
     }
 
-};
+    else {
+        guessesLeft--;
+        guessedLetters.push(userGuess);
+        // document.querySelector('#losses').innerHTML = "Losses: " + losses;
+        updateGuessesSoFar();
+        updateGuessesLeft();
+        if (guessesLeft === 0) {
+            losses++;
+            document.querySelector('#losses').innerHTML = "losses: " + losses;
+            reset();
+        } else {
+
+        }
+    }
 }
-     
-      
-
-
-
-
-
- 
 
 
 
@@ -93,14 +72,21 @@ console.log ("solon")
 
 
 
-//     // HTML now keeps track of the user and computer guesses and wins and losses.
-//     var html =
-//         "<p>You chose: " + userGuess + "</p>" +
-//         "<p>The computer chose: " + computerGuess + "</p>" +
-//         "<p>wins: " + wins + "</p>" +
-//         "<p>losses: " + losses + "</p>" +
-//         "<p>ties: " + ties + "</p>";
 
-//     // document.querySelector("#game").innerHTML = html;  
-//     document.querySelector("#game").innerHTML = html;
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
